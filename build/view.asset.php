d2b23622b4fad2cb6f4de2a42ec0b6d27c459009
James Shakespeare <j@jshakespeare.com>
2024-09-26 10:46:13 -0400

    Wafer canvas refactor (#4)
    * refactor to use <canvas> for visualizing wafer
    
    * simplify canvas component file
    
    * canvas scale and layout fixes for smaller wafers
    
    * tweak scale and layout further

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '8bb8968c26e29b6c3367');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '2ec6f76d94a7b4869ef2');
--- src/components/WaferCanvas/WaferCanvas.tsx
+++ src/components/WaferCanvas/WaferCanvas.tsx
@@ -1,62 +1,80 @@
-import React from "react";
-import { Die, FabResults } from "../../types";
+import React, { useEffect, useRef } from "react";
+import { Die, FabResults, WaferShape } from "../../types";
 
-function DieItem(props: Die) {
-	const stateColors = {
+const mmToPxScale = 3;
+
+function DieMapCanvas(props: { results: FabResults }) {
+	// Don't try and draw too many dies, or performance will suffer too much and the
+	// page may hang or crash
+	const maxDies = 100000;
+	const canvasEl = useRef<HTMLCanvasElement>(null);
+	const dieStateColors = {
 		good: "rgba(6,231,6,0.77)",
 		defective: "rgba(151,138,129,0.8)",
 		partial: "yellow",
 		lost: "red"
 	};
 
-	return (
-		<rect
-			x={props.x}
-			y={props.y}
-			width={props.width}
-			height={props.height}
-			fill={stateColors[props.dieState]}
-		/>
-	);
-}
+	useEffect(() => {
+		if (!canvasEl.current || !props.results.dies.length || props.results.dies.length > maxDies) {
+			return;
+		}
 
-export function DiscCanvas(props: { results: FabResults }) {
-	// Bail out if there are too many dies to draw, otherwise the browser will hang
-	if (props.results.totalDies > 9999) {
-		return "Too many dies to visualize";
+		const context = canvasEl.current.getContext("2d");
+
+		if (!context) {
+			return;
+		}
+
+		// Clear the canvas before drawing new die map
+		context.clearRect(0, 0, canvasEl.current.width, canvasEl.current.height);
+
+		// Draw each die onto the canvas
+		props.results.dies.forEach((die) => {
+			context.fillStyle = dieStateColors[die.dieState];
+			context.fillRect(
+				mmToPxScale * die.x,
+				mmToPxScale * die.y,
+				mmToPxScale * die.width,
+				mmToPxScale * die.height,
+			);
+		});
+	}, [JSON.stringify(props.results)]);
+
+	if (props.results.dies.length > maxDies) {
+		return (
+			<div
+				className="too-many-dies"
+				style={{
+					paddingBottom: `${props.results.waferWidth / props.results.waferHeight * 100}%`
+				}}
+			>
+				<span>Too many dies to visualize</span>
+			</div>
+		);
 	}
 
 	return (
-		<svg
-			viewBox={`0 0 ${props.results.waferWidth} ${props.results.waferWidth}`}
-			className="wafer-canvas disc"
-		>
-			<circle
-				cx={props.results.waferWidth / 2}
-				cy={props.results.waferWidth / 2}
-				r={Math.min(props.results.waferWidth, props.results.waferWidth) / 2}
-				stroke="none"
-				fill="none"
-			/>
-			{
-				props.results.dies.map((die) => (<DieItem {...die} />))
-			}
-		</svg>
+		<canvas
+			ref={canvasEl}
+			width={props.results.waferWidth * mmToPxScale}
+			height={props.results.waferHeight * mmToPxScale}
+		></canvas>
 	);
 }
 
-export function PanelCanvas(props: { results: FabResults }) {
-	// Bail out if there are too many dies to draw, otherwise the browser will hang
-	if (props.results.totalDies > 9999) {
-		return "Too many dies to visualize";
-	}
-
+/**
+ * Draw a die map onto either a circular or rectangular background, depending on
+ * the shape of the wafer. Dies are drawn using <canvas> and colored according
+ * to their state (good, defective, etc.)
+ */
+export function WaferCanvas(props: {
+	results: FabResults,
+	shape: WaferShape
+}) {
 	return (
-		<svg viewBox={`0 0 ${props.results.waferWidth} ${props.results.waferWidth}`} className="wafer-canvas">
-			{
-				props.results.dies.map((die) => <DieItem {...die} />)
-			}
-		</svg>
+		<div className={`wafer-canvas ${props.shape === 'Disc' ? 'disc' : ''}`}>
+			<DieMapCanvas results={props.results} />
+		</div>
 	);
 }
-
