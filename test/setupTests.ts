1c69bc35457b3563705c3775166ff521ff74a5b2
James Shakespeare <j@jshakespeare.com>
2024-10-08 17:08:52 +0100

    mock intersection observer for jest

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,3 +3,11 @@ import "@testing-library/jest-dom";
 
 // For mocking the <canvas> element
 import 'jest-canvas-mock';
+
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+	observe: () => null,
+	unobserve: () => null,
+});
+
+window.IntersectionObserver = mockIntersectionObserver;
