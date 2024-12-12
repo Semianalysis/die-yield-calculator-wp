dd460d0bf518caead3d2c6287caad28101ae1742
James Shakespeare <j@jshakespeare.com>
2024-12-12 14:27:25 +0000

    reset block version bump

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '72210200c739bd668d0e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bed116b4c5a260bce299');
