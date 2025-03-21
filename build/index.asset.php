a45f1b90901eb5efd467eb4f6aee878b260c11ff
James Shakespeare <j@jshakespeare.com>
2025-03-21 15:15:44 +0000

    bump plugin and block version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '10030dccad8e86321766');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '44f40cee5a1b0d51dcdb');
