559f0547223eb27ba5dfa3de63b279e0f0854ef4
James Shakespeare <j@jshakespeare.com>
2025-03-21 15:45:41 +0000

    prevent parent site styles affecting <select> text size

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '44f40cee5a1b0d51dcdb');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '998d85251bb67e8156ce');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'f41eb8550d3eda57fa59');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '1c426b6e0d58256d94ee');
