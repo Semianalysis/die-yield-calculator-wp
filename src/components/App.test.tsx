42d745699108cf6fc370b754c5551ae4d4eda654
James Shakespeare <j@jshakespeare.com>
2025-05-12 15:03:44 +0100

    update tests with new logic

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { act } from "react";
 import App from "./App";
 import { render, screen, waitFor, within } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
@@ -6,22 +6,30 @@ import { defaultFieldWidth, defaultFieldHeight } from "../config";
 
 describe("App", () => {
 	it("calculates the correct number of total 5mm dies on a 300mm panel with no scribe lines", async () => {
+		let excludedCountNum = 0;
+		let totalCountNum = 0;
+
 		render(<App />);
 		const user = userEvent.setup();
-		await user.click(screen.getByRole("radio", {
-			name: /Panel/
-		}));
+
+		await user.click(
+			screen.getByRole("radio", {
+				name: /Panel/,
+			}),
+		);
 
 		const widthInput = screen.getByRole("spinbutton", {
-			name: /Width/
+			name: /Width/,
 		});
 		const scribeLinesXInput = screen.getByRole("spinbutton", {
-			name: /Scribe Line Minimum X/
+			name: /Scribe Line Minimum X/,
 		});
 		const scribeLinesYInput = screen.getByRole("spinbutton", {
-			name: /Scribe Line Minimum Y/
+			name: /Scribe Line Minimum Y/,
 		});
-		const maintainAspectRatioCheckbox = screen.getByRole("checkbox", { name: /Aspect Ratio/ });
+		const maintainAspectRatioCheckbox = screen.getByRole("checkbox", {
+			name: /Aspect Ratio/,
+		});
 
 		await user.click(maintainAspectRatioCheckbox);
 		await user.clear(widthInput);
@@ -31,30 +39,53 @@ describe("App", () => {
 		await user.clear(scribeLinesYInput);
 		await user.type(scribeLinesYInput, "0");
 
-		// How many 26mm x 33mm field shots can we fit in the panel?
-		const fieldCountX = Math.ceil(300 / 26);
-		const fieldCountY = Math.ceil(300 / 33);
-		const fieldCount = fieldCountX * fieldCountY;
-		// How many 5mm square dies can we fit in a single field shot?
-		const dieCountX = Math.floor(26 / 5);
-		const dieCountY = Math.floor(33 / 5);
-		const dieCount = dieCountX * dieCountY;
-		// How many dies can we fit in the entire panel?
-		const totalDieCount = fieldCount * dieCount;
+		await waitFor(async () => {
+			const totalTextNode = await screen.findByText(/Total Dies/);
 
-		expect(await screen.findByText(new RegExp(totalDieCount.toString()))).toBeInTheDocument();
+			if (totalTextNode.textContent) {
+				// Wait for the calculation to appear
+				const countStr = await within(totalTextNode).findByText(/\d+/);
+
+				if (countStr.textContent) {
+					const countMatch = countStr.textContent.match(/\d+/)?.[0];
+					totalCountNum = countMatch ? parseInt(countMatch) : 0;
+				}
+			}
+
+			const excludedTextNode = await screen.findByText(/Excluded Dies/);
+
+			if (excludedTextNode.textContent) {
+				// Wait for the calculation to appear
+				const countStr = await within(excludedTextNode).findByText(/\d+/);
+
+				if (countStr.textContent) {
+					const countMatch = countStr.textContent.match(/\d+/)?.[0];
+					excludedCountNum = countMatch ? parseInt(countMatch) : 0;
+				}
+			}
+
+			// How many die can we fit in the entire panel?
+			const expectedTotalDieCount = 300 * 300 / (5 * 5);
+
+			expect(totalCountNum - excludedCountNum).toEqual(expectedTotalDieCount);
+		}, { timeout: 200 });
 	});
 
 	it("calculates yields for wafer shape", async () => {
 		render(<App />);
 		const user = userEvent.setup();
-		await user.click(screen.getByRole("radio", {
-			name: /Wafer/
-		}));
+		await user.click(
+			screen.getByRole("radio", {
+				name: /Wafer/,
+			}),
+		);
 		// No notch keepout
-		await user.type(screen.getByRole("spinbutton", {
-			name: /Notch keep-out/
-		}), "{backspace}0");
+		await user.type(
+			screen.getByRole("spinbutton", {
+				name: /Notch keep-out/,
+			}),
+			"{backspace}0",
+		);
 		await waitFor(() => expect(screen.getByText(/1104/)).toBeInTheDocument());
 	});
 
@@ -64,32 +95,30 @@ describe("App", () => {
 		// Get the number of dies displayed for each state and the total number of dies.
 		let totalDiesCount = 0;
 		let allStatesCount = 0;
-		await Promise.all([
-			"Total",
-			"Good",
-			"Defective",
-			"Partial",
-			"Excluded"
-		].map(async (label) => {
-			const regex = new RegExp(`${label} Dies`);
-			const textNode = await screen.findByText(regex);
+		await Promise.all(
+			["Total", "Good", "Defective", "Partial", "Excluded"].map(
+				async (label) => {
+					const regex = new RegExp(`${label} Dies`);
+					const textNode = await screen.findByText(regex);
 
-			if (textNode.textContent) {
-				// Wait for the calculation to appear
-				const countStr = await within(textNode).findByText(/\d+/);
+					if (textNode.textContent) {
+						// Wait for the calculation to appear
+						const countStr = await within(textNode).findByText(/\d+/);
 
-				if (countStr.textContent) {
-					const countMatch = countStr.textContent.match(/\d+/)?.[0];
-					const countNum = countMatch ? parseInt(countMatch) : 0;
+						if (countStr.textContent) {
+							const countMatch = countStr.textContent.match(/\d+/)?.[0];
+							const countNum = countMatch ? parseInt(countMatch) : 0;
 
-					if (label === "Total") {
-						totalDiesCount = countNum;
-					} else {
-						allStatesCount += countNum;
+							if (label === "Total") {
+								totalDiesCount = countNum;
+							} else {
+								allStatesCount += countNum;
+							}
+						}
 					}
-				}
-			}
-		}));
+				},
+			),
+		);
 
 		expect(totalDiesCount).toBeGreaterThan(0);
 		expect(totalDiesCount).toEqual(allStatesCount);
@@ -98,7 +127,9 @@ describe("App", () => {
 	it("automatically adjusts the other die dimension input when one is changed with maintain aspect ratio on", async () => {
 		render(<App />);
 		const user = userEvent.setup();
-		const maintainAspectRatioCheckbox = screen.getByRole("checkbox", { name: /Aspect Ratio/ });
+		const maintainAspectRatioCheckbox = screen.getByRole("checkbox", {
+			name: /Aspect Ratio/,
+		});
 		const dieWidthInput = screen.getByRole("spinbutton", { name: /Width/ });
 		const dieHeightInput = screen.getByRole("spinbutton", { name: /Height/ });
 
@@ -141,15 +172,23 @@ describe("App", () => {
 	it("shows how many full and partial shots will be taken and how many die fit on a reticle", async () => {
 		render(<App />);
 		const totalDiesNode = await screen.findByText(/Total Dies: [0-9]+/);
-		const totalDies = parseInt(totalDiesNode.textContent?.match(/\d+/)?.[0] || "0");
+		const totalDies = parseInt(
+			totalDiesNode.textContent?.match(/\d+/)?.[0] || "0",
+		);
 
 		// Get the number of dies per shot
-		const diePerReticleNode = await screen.findByText(/Die Per Reticle: [0-9]+/);
-		const diePerReticle = parseInt(diePerReticleNode.textContent?.match(/\d+/)?.[0] || "0");
+		const diePerReticleNode = await screen.findByText(
+			/Die Per Reticle: [0-9]+/,
+		);
+		const diePerReticle = parseInt(
+			diePerReticleNode.textContent?.match(/\d+/)?.[0] || "0",
+		);
 
 		// Get the number of shots
 		const shotCountNode = await screen.findByText(/Exposures: [0-9]+/);
-		const shotCount = parseInt(shotCountNode.textContent?.match(/\d+/)?.[0] || "0");
+		const shotCount = parseInt(
+			shotCountNode.textContent?.match(/\d+/)?.[0] || "0",
+		);
 
 		// The total number of dies should be the number of dies per shot times the number of shots
 		expect(totalDies).toEqual(diePerReticle * shotCount);
@@ -163,7 +202,9 @@ describe("App", () => {
 		const dieWidthInput = screen.getByRole("spinbutton", { name: /Width/ });
 		const dieHeightInput = screen.getByRole("spinbutton", { name: /Height/ });
 		const reticleLimitCheckbox = screen.getByRole("checkbox", {
-			name: new RegExp(`Reticle Limit \\(${defaultFieldWidth}mm x ${defaultFieldHeight}mm\\)`)
+			name: new RegExp(
+				`Reticle Limit \\(${defaultFieldWidth}mm x ${defaultFieldHeight}mm\\)`,
+			),
 		});
 
 		// By default, Reticle Limit should be checked
@@ -179,7 +220,9 @@ describe("App", () => {
 		// Wait for validation error to appear
 		await waitFor(() => {
 			const errorText = screen.getByText(
-				new RegExp(`Die and scribe line width must be less than or equal to the field width \\(${defaultFieldWidth}mm\\)`)
+				new RegExp(
+					`Die and scribe line width must be less than or equal to the field width \\(${defaultFieldWidth}mm\\)`,
+				),
 			);
 			expect(errorText).toBeInTheDocument();
 		});
@@ -193,7 +236,9 @@ describe("App", () => {
 		await user.type(dieWidthInput, (defaultFieldWidth + 10).toString());
 
 		// Input should accept the larger value
-		expect(dieWidthInput).toHaveDisplayValue((defaultFieldWidth + 10).toString());
+		expect(dieWidthInput).toHaveDisplayValue(
+			(defaultFieldWidth + 10).toString(),
+		);
 
 		// Wait for the validation error to disappear
 		await waitFor(() => {
@@ -207,7 +252,9 @@ describe("App", () => {
 		await user.type(dieHeightInput, (defaultFieldHeight + 10).toString());
 
 		// Input should accept the larger value
-		expect(dieHeightInput).toHaveDisplayValue((defaultFieldHeight + 10).toString());
+		expect(dieHeightInput).toHaveDisplayValue(
+			(defaultFieldHeight + 10).toString(),
+		);
 
 		// Check the Reticle Limit checkbox again
 		await user.click(reticleLimitCheckbox);
@@ -220,7 +267,9 @@ describe("App", () => {
 		// And validation error should reappear
 		await waitFor(() => {
 			// We should see at least one validation error about field dimensions
-			const errorText = screen.getByText(/must be less than or equal to the field (width|height)/i);
+			const errorText = screen.getByText(
+				/must be less than or equal to the field (width|height)/i,
+			);
 			expect(errorText).toBeInTheDocument();
 		});
 	});
@@ -236,34 +285,63 @@ describe("App", () => {
 			"Manual",
 		];
 
-		it.each(yieldModelOptions)("shows the correct inputs for %s", async (model) => {
-			render(<App />);
-			const user = userEvent.setup();
-			const yieldModelSelect = await screen.findByRole("combobox", { name: "Yield Calculation Model" });
-			await user.selectOptions(yieldModelSelect, model);
-			switch (model) {
-				case "Poisson Model":
-				case "Murphy's Model":
-				case "Rectangular Model":
-				case "Moore's Model":
-				case "Seeds Model":
-					expect(screen.queryByRole("spinbutton", { name: /Defect Rate/ })).toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Die Area/ })).toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Manual Yield/ })).not.toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Layers/ })).not.toBeInTheDocument();
-					break;
-				case "Bose-Einstein Model":
-					expect(screen.queryByRole("spinbutton", { name: /Defect Rate/ })).toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Die Area/ })).toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Manual Yield/ })).not.toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Layers/ })).toBeInTheDocument();
-					break;
-				case "Manual":
-					expect(screen.queryByRole("spinbutton", { name: /Yield/ })).toBeInTheDocument()
-					expect(screen.queryByRole("spinbutton", { name: /Defect Rate/ })).not.toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Die Area/ })).not.toBeInTheDocument();
-					expect(screen.queryByRole("spinbutton", { name: /Critical Layers/ })).not.toBeInTheDocument();
-			}
-		});
-	})
+		it.each(yieldModelOptions)(
+			"shows the correct inputs for %s",
+			async (model) => {
+				render(<App />);
+				const user = userEvent.setup();
+				const yieldModelSelect = await screen.findByRole("combobox", {
+					name: "Yield Calculation Model",
+				});
+				await user.selectOptions(yieldModelSelect, model);
+				switch (model) {
+					case "Poisson Model":
+					case "Murphy's Model":
+					case "Rectangular Model":
+					case "Moore's Model":
+					case "Seeds Model":
+						expect(
+							screen.queryByRole("spinbutton", { name: /Defect Rate/ }),
+						).toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Die Area/ }),
+						).toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Manual Yield/ }),
+						).not.toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Layers/ }),
+						).not.toBeInTheDocument();
+						break;
+					case "Bose-Einstein Model":
+						expect(
+							screen.queryByRole("spinbutton", { name: /Defect Rate/ }),
+						).toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Die Area/ }),
+						).toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Manual Yield/ }),
+						).not.toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Layers/ }),
+						).toBeInTheDocument();
+						break;
+					case "Manual":
+						expect(
+							screen.queryByRole("spinbutton", { name: /Yield/ }),
+						).toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Defect Rate/ }),
+						).not.toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Die Area/ }),
+						).not.toBeInTheDocument();
+						expect(
+							screen.queryByRole("spinbutton", { name: /Critical Layers/ }),
+						).not.toBeInTheDocument();
+				}
+			},
+		);
+	});
 });
