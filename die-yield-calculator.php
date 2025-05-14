5dbed86e46ed1ddea1db2e49b791aac6ad667ed3
James Shakespeare <j@jshakespeare.com>
2025-05-14 07:12:14 +0100

    v.0.3.0

--- die-yield-calculator.php
+++ die-yield-calculator.php
@@ -4,7 +4,7 @@
  * Description:       Gutenberg block for embedding the SemiAnalysis die yield calculator React application in posts and pages.
  * Requires at least: 6.6
  * Requires PHP:      7.0
- * Version:           0.2.3
+ * Version:           0.3.0
  * Author:            SemiAnalysis
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
